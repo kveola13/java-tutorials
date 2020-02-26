0af1c3acc8d1fb47be785d5f8c5bc649b91911a7
ola <kveola13@student.westerdals.no>
2020-02-26 22:38:30 +0100

    Added a json reader

---
+++ src/main/java/com/kveola/intermediate/ReadJson.java
@@ -0,0 +1,26 @@
+package com.kveola.intermediate;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class ReadJson {
+
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            Gson gson = new Gson();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String parsedText = "";
+            while (null != (parsedText = bufferedReader.readLine())) {
+                System.out.println(parsedText);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
