5b00a1d3feb964c7ff36ae41c6850bae354b07bc
olakv <kveola13@student.westerdals.no>
2020-03-18 22:56:18 +0100

    Started cb tasks

---
+++ src/main/java/com/kveola/cb/FrontThree.java
@@ -0,0 +1,13 @@
+package com.kveola.cb;
+
+public class FrontThree {
+    public static String front3(String str) {
+        return str.length() > 3 ? str.substring(0,3).repeat(3) : str.repeat(3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(front3("Java"));
+        System.out.println(front3("Chocolate"));
+        System.out.println(front3("abc"));
+    }
+}
