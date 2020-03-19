da47111775ceedf12ba814478adcbd4570c84570
olakv <kveola13@student.westerdals.no>
2020-03-20 00:03:13 +0100

    Started cb tasks

---
+++ src/main/java/com/kveola/cb/SoloTeen.java
@@ -0,0 +1,13 @@
+package com.kveola.cb;
+
+public class SoloTeen {
+    public static boolean loneTeen(int a, int b) {
+        return (a >= 13 && a <= 19) && !(b >= 13 && b <= 19) || !(a >= 13 && a <= 19) && (b >= 13 && b <= 19);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(loneTeen(13, 99));
+        System.out.println(loneTeen(21, 19));
+        System.out.println(loneTeen(13, 13));
+    }
+}
