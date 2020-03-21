80ec56a5e1483a3b1658a0b4f8f4981c0df882a0
olakv <kveola13@student.westerdals.no>
2020-03-21 18:05:19 +0100

    added the endUp task

---
+++ src/main/java/com/kveola/cb/UpperLast.java
@@ -0,0 +1,7 @@
+package com.kveola.cb;
+
+public class UpperLast {
+    public static String endUp(String str) {
+        return str.length() < 3 ? str.toUpperCase() : str.substring(0, str.length() - 3) + (str.substring(str.length() - 3, str.length()).toUpperCase());
+    }
+}
