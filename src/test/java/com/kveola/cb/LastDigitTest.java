204e09d9dad667f8fbf2b71ff7dcb86fef9fbdd1
olakv <kveola13@student.westerdals.no>
2020-03-21 17:17:29 +0100

    added the LastDigitTask and the StringE task

---
+++ src/test/java/com/kveola/cb/LastDigitTest.java
@@ -0,0 +1,28 @@
+package com.kveola.cb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LastDigitTest {
+
+    @Test
+    void lastDigitTestOne() {
+        assertTrue(LastDigit.lastDigit(7, 17));
+    }
+
+    @Test
+    void lastDigitTestTwo() {
+        assertFalse(LastDigit.lastDigit(6, 17));
+    }
+
+    @Test
+    void lastDigitTestThree() {
+        assertTrue(LastDigit.lastDigit(3, 113));
+    }
+
+    @Test
+    void lastDigitTestFour() {
+        assertTrue(LastDigit.lastDigit(7, 17));
+    }
+}
