bf949d936dc1011c5f5bca46c6e456d7f1d040d7
olakv <kveola13@student.westerdals.no>
2020-03-22 11:18:34 +0100

    added everyNth task

---
+++ src/test/java/com/kveola/cb/PrintEveryXTest.java
@@ -0,0 +1,23 @@
+package com.kveola.cb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintEveryXTest {
+
+    @Test
+    void everyNthTestOne() {
+        assertEquals("Mrce", PrintEveryX.everyNth("Miracle", 2));
+    }
+
+    @Test
+    void everyNthTestTwo() {
+        assertEquals("aceg", PrintEveryX.everyNth("abcdefg", 2));
+    }
+
+    @Test
+    void everyNthTestThree() {
+        assertEquals("adg", PrintEveryX.everyNth("abcdefg", 3));
+    }
+}
