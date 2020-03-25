5d2e30da47f3dd9a1a66e5c9cf35d58c0bb27435
ola <kveola13@student.westerdals.no>
2020-03-25 18:16:20 +0100

    refactored

--- src/main/java/com/kveola/cb/warmupTwo/StringMatch.java
+++ src/main/java/com/kveola/cb/warmupTwo/StringMatch.java
@@ -3,15 +3,10 @@ package com.kveola.cb.warmupTwo;
 public class StringMatch {
     public static int stringMatch(String a, String b) {
         int counter = 0;
-        for (int i = 0; i < a.length() - 1; i++) {
-            for (int j = 0; j < b.length() - 1; j++) {
-                if (a.substring(i, i + 2).equals(b.substring(j, j + 2))) {
-                    /*System.out.println("A");
-                    System.out.println(a.substring(i, i + 2));
-                    System.out.println("B");
-                    System.out.println(b.substring(j, j + 2));*/
-                    counter++;
-                }
+        int shortestLength = Math.min(a.length(), b.length());
+        for (int i = 0; i < shortestLength - 1; i++) {
+            if(a.substring(i, i+2).equals(b.substring(i, i+2))){
+                counter++;
             }
         }
         return counter;
