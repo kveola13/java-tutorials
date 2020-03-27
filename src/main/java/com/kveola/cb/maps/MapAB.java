07d6b5d6b1ea2b2b65a690b248908b44238a7df9
olakv <kveola13@student.westerdals.no>
2020-03-27 15:55:16 +0100

    added the mapAB4 task

--- src/main/java/com/kveola/cb/maps/MapAB.java
+++ src/main/java/com/kveola/cb/maps/MapAB.java
@@ -4,26 +4,40 @@ import java.util.Map;
 
 public class MapAB {
     public static Map<String, String> mapAB(Map<String, String> map) {
-        if(map.containsKey("a") && map.containsKey("b")){
-            map.put("ab", map.get("a")+map.get("b"));
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
         }
         return map;
     }
+
     public static Map<String, String> mapAB2(Map<String, String> map) {
-        if(map.containsKey("a")&& map.containsKey("b")){
-            if(map.get("a").equals(map.get("b"))){
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").equals(map.get("b"))) {
                 map.remove("a");
                 map.remove("b");
             }
         }
         return map;
     }
+
     public static Map<String, String> mapAB3(Map<String, String> map) {
-        if (map.containsKey("a") && !map.containsKey("b")){
+        if (map.containsKey("a") && !map.containsKey("b")) {
             map.put("b", map.get("a"));
-        }
-        else if (map.containsKey("b") && !map.containsKey("a")){
+        } else if (map.containsKey("b") && !map.containsKey("a")) {
             map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
+    public static Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() == map.get("b").length()) {
+                map.put("a", "");
+                map.put("b", "");
+            } else {
+                boolean aIsLongest = Math.max(map.get("a").length(), map.get("b").length()) == map.get("a").length();
+                map.put("c", aIsLongest ? map.get("a") : map.get("b"));
+            }
         }
         return map;
     }
--- src/test/java/com/kveola/cb/maps/MapABTest.java
+++ src/test/java/com/kveola/cb/maps/MapABTest.java
@@ -81,4 +81,25 @@ class MapABTest {
                 (Map.of("a", "aaa", "b", "bbb", "c", "cake"));
         assertEquals(solvedMap, mapAB3(initMap));
     }
+    @Test
+    void mapAB4TestOne() {
+        Map<String, String> initMap = new HashMap<>(Map.of("a", "aaa", "b", "bb", "c", "cake"));
+        Map<String, String> solvedMap = new HashMap<>
+                (Map.of("a", "aaa", "b", "bb", "c", "aaa"));
+        assertEquals(solvedMap, mapAB4(initMap));
+    }
+    @Test
+    void mapAB4TestTwo() {
+        Map<String, String> initMap = new HashMap<>(Map.of("a", "aa", "b", "bbb", "c", "cake"));
+        Map<String, String> solvedMap = new HashMap<>
+                (Map.of("a", "aa", "b", "bbb", "c", "bbb"));
+        assertEquals(solvedMap, mapAB4(initMap));
+    }
+    @Test
+    void mapAB4TestThree() {
+        Map<String, String> initMap = new HashMap<>(Map.of("a", "aa", "b", "bbb"));
+        Map<String, String> solvedMap = new HashMap<>
+                (Map.of("a", "aa", "b", "bbb", "c", "bbb"));
+        assertEquals(solvedMap, mapAB4(initMap));
+    }
 }
