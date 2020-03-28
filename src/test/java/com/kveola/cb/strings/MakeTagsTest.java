7e0b73e3a5fbbe41db273848270e98f4c1323a99
ola <kveola13@student.westerdals.no>
2020-03-28 14:09:05 +0100

    added the MakeTags task

---
+++ src/test/java/com/kveola/cb/strings/MakeTagsTest.java
@@ -0,0 +1,22 @@
+package com.kveola.cb.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kveola.cb.strings.MakeTags.makeTags;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MakeTagsTest {
+
+    @Test
+    void makeTagsTestOne() {
+        assertEquals("<i>Yay</i>", makeTags("i", "Yay"));
+    }
+    @Test
+    void makeTagsTestTwo() {
+        assertEquals("<i>Hello</i>", makeTags("i", "Hello"));
+    }
+    @Test
+    void makeTagsTestThree() {
+        assertEquals("<cite>Yay</cite>", makeTags("cite", "Yay"));
+    }
+}
