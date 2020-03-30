dabffef8f03e316acb772837263d76f22e77a5ce
olakv <kveola13@student.westerdals.no>
2020-03-30 18:32:07 +0200

    added WithoutEnd2 task

--- src/main/java/com/kveola/cb/strings/WithoutEnd.java
+++ src/main/java/com/kveola/cb/strings/WithoutEnd.java
@@ -4,4 +4,8 @@ public class WithoutEnd {
     public static String withoutEnd(String str) {
         return str.length() >= 2 ? str.substring(1, str.length() - 1) : "incorrect length";
     }
+
+    public static String withouEnd2(String str) {
+        return str.length() > 2 ? str.substring(1, str.length() - 1) : "";
+    }
 }
--- src/test/java/com/kveola/cb/strings/WithoutEndTest.java
+++ src/test/java/com/kveola/cb/strings/WithoutEndTest.java
@@ -2,6 +2,7 @@ package com.kveola.cb.strings;
 
 import org.junit.jupiter.api.Test;
 
+import static com.kveola.cb.strings.WithoutEnd.withouEnd2;
 import static com.kveola.cb.strings.WithoutEnd.withoutEnd;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +21,20 @@ class WithoutEndTest {
     @Test
     void withoutEndTestThree() {
         assertEquals("odin", withoutEnd("Coding"));
+    }
+
+    @Test
+    void withoutEnd2TestOne() {
+        assertEquals("ell", withouEnd2("Hello"));
+    }
+
+    @Test
+    void withoutEnd2TestTwo() {
+        assertEquals("b", withouEnd2("abc"));
+    }
+
+    @Test
+    void withoutEnd2TestThree() {
+        assertEquals("", withouEnd2("ab"));
     }
 }
