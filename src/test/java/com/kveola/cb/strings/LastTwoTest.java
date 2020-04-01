50c998ecdbcd7af2c66346b0ad734cc8bc37d3ca
olakv <kveola13@student.westerdals.no>
2020-04-01 15:56:01 +0200

    added the LastTwo task

---
+++ src/test/java/com/kveola/cb/strings/LastTwoTest.java
@@ -0,0 +1,30 @@
+package com.kveola.cb.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kveola.cb.strings.LastTwo.lastTwo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LastTwoTest {
+
+    @Test
+    void lastTwoTestOne() {
+        assertEquals("codign", lastTwo("coding"));
+    }
+    @Test
+    void lastTwoTestTwo() {
+        assertEquals("cta", lastTwo("cat"));
+    }
+    @Test
+    void lastTwoTestThree() {
+        assertEquals("ab", lastTwo("ba"));
+    }
+    @Test
+    void lastTwoTestFour() {
+        assertEquals("a", lastTwo("a"));
+    }
+    @Test
+    void lastTwoTestFive() {
+        assertEquals("", lastTwo(""));
+    }
+}
