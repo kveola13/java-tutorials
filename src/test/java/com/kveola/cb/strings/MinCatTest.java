4d8c63eb903d0696cf2f430de69d5cfd5c864de0
olakv <kveola13@student.westerdals.no>
2020-04-01 16:28:29 +0200

    added the MinCat task

---
+++ src/test/java/com/kveola/cb/strings/MinCatTest.java
@@ -0,0 +1,24 @@
+package com.kveola.cb.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kveola.cb.strings.MinCat.minCat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinCatTest {
+
+    @Test
+    void minCatTestOne() {
+        assertEquals("loHi", minCat("Hello", "Hi"));
+    }
+
+    @Test
+    void minCatTestTwo() {
+        assertEquals("ellojava", minCat("Hello", "java"));
+    }
+
+    @Test
+    void minCatTestThree() {
+        assertEquals("javaello", minCat("java", "Hello"));
+    }
+}
