aee8d832d5ec26cac1b57e2eebc13223c8444336
olakv <kveola13@student.westerdals.no>
2020-04-05 13:40:26 +0200

    added the DoubleTwoThree task

---
+++ src/main/java/com/kveola/cb/arrays/DoubleTwoThree.java
@@ -0,0 +1,8 @@
+package com.kveola.cb.arrays;
+
+public class DoubleTwoThree {
+    public static boolean double23(int[] nums) {
+        if (nums.length < 2) return false;
+        return nums[0] == 2 && nums[1] == 2 || nums[0] == 3 && nums[1] == 3;
+    }
+}
