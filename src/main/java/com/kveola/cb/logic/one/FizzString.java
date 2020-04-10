e9209db09aaca0eab8b3dfe4ec8587754ddbdc54
olakv <kveola13@student.westerdals.no>
2020-04-10 19:31:36 +0200

    added FizzString2 task

--- src/main/java/com/kveola/cb/logic/one/FizzString.java
+++ src/main/java/com/kveola/cb/logic/one/FizzString.java
@@ -8,4 +8,12 @@ public class FizzString {
         if (!str.startsWith("f") && !str.endsWith("b")) return str;
         return stringToReturn;
     }
+
+    public static String fizzString2(int n) {
+        String stringToReturn = "";
+        if (n % 3 == 0) stringToReturn += "Fizz";
+        if (n % 5 == 0) stringToReturn += "Buzz";
+        if (n % 3 != 0 && n % 5 != 0) return n + "!";
+        return stringToReturn + "!";
+    }
 }
--- src/test/java/com/kveola/cb/logic/one/FizzStringTest.java
+++ src/test/java/com/kveola/cb/logic/one/FizzStringTest.java
@@ -3,6 +3,7 @@ package com.kveola.cb.logic.one;
 import org.junit.jupiter.api.Test;
 
 import static com.kveola.cb.logic.one.FizzString.fizzString;
+import static com.kveola.cb.logic.one.FizzString.fizzString2;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzStringTest {
@@ -20,5 +21,20 @@ class FizzStringTest {
     @Test
     void fizzStringTestThree() {
         assertEquals("FizzBuzz", fizzString("fib"));
+    }
+
+    @Test
+    void fizzString2TestOne() {
+        assertEquals("1!", fizzString2(1));
+    }
+
+    @Test
+    void fizzString2TestTwo() {
+        assertEquals("2!", fizzString2(2));
+    }
+
+    @Test
+    void fizzString2TestThree() {
+        assertEquals("Fizz!", fizzString2(3));
     }
 }
