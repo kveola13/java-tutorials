c1d3f4c6265040b5982e995b1acdee0bf1591dfc
olakv <kveola13@student.westerdals.no>
2020-04-11 12:17:50 +0200

    refactoring

--- src/main/java/com/kveola/cb/logic/one/LessByTen.java
+++ src/main/java/com/kveola/cb/logic/one/LessByTen.java
@@ -2,6 +2,6 @@ package com.kveola.cb.logic.one;
 
 public class LessByTen {
     public static boolean lessBy10(int a, int b, int c) {
-        return a <= b - 10 || b <= c - 10 || c <= a - 10 || a <= c - 10 || b <= a - 10 || c <= b - 10;
+        return Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10 || Math.abs(a - c) >= 10;
     }
 }
