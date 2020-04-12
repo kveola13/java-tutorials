cbba7b82054a1c18c810e9a75ab5333ed5e02e4c
olakv <kveola13@student.westerdals.no>
2020-04-13 00:21:30 +0200

    refactoring

--- src/main/java/com/kveola/cb/logic/one/ShareDigit.java
+++ src/main/java/com/kveola/cb/logic/one/ShareDigit.java
@@ -4,7 +4,8 @@ public class ShareDigit {
     public static boolean shareDigit(int a, int b) {
         boolean shareLeft = a % 10 == b % 10;
         boolean shareRight = a % 10 == b / 10;
-        boolean shareLeftRight = b % 10 == a / 10;
-        return shareLeft || shareRight || shareLeftRight;
+        boolean shareLeftRight = a / 10 == b / 10;
+        boolean shareRightLeft = a / 10 == b % 10;
+        return shareLeft || shareRight || shareLeftRight || shareRightLeft;
     }
 }
