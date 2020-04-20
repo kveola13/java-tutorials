96dc47a65ca898b624b13d46db10be42c6f067f5
olakv <kveola13@student.westerdals.no>
2020-04-21 00:02:06 +0200

    added SumDigits task

---
+++ src/main/java/com/kveola/cb/strings/three/SumDigits.java
@@ -0,0 +1,13 @@
+package com.kveola.cb.strings.three;
+
+public class SumDigits {
+    public static int sumDigits(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i, i + 1).matches("[0-9]")) {
+                sum += Integer.parseInt(str.substring(i, i + 1));
+            }
+        }
+        return sum;
+    }
+}
