4e1e37039f68d7d50a6de186258296e6985b2ba4
ola <kveola13@student.westerdals.no>
2020-04-24 16:37:09 +0200

    refactoring

--- src/test/java/com/kveola/cb/arrays/two/More14Test.java
+++ src/test/java/com/kveola/cb/arrays/two/More14Test.java
@@ -2,23 +2,23 @@ package com.kveola.cb.arrays.two;
 
 import org.junit.jupiter.api.Test;
 
-import static com.kveola.cb.arrays.two.Only14.only14;
+import static com.kveola.cb.arrays.two.More14.more14;
 import static org.junit.jupiter.api.Assertions.*;
 
 class only14Test {
 
     @Test
-    void only14TestOne() {
-        assertTrue(only14(new int[]{1, 4, 1, 4}));
+    void more14TestOne() {
+        assertTrue(more14(new int[]{1, 4, 1}));
     }
 
     @Test
-    void only14TestTwo() {
-        assertFalse(only14(new int[]{1, 4, 2, 4}));
+    void more14TestTwo() {
+        assertFalse(more14(new int[]{1, 4, 2, 4}));
     }
 
     @Test
-    void only14TestThree() {
-        assertTrue(only14(new int[]{1, 1}));
+    void more14TestThree() {
+        assertTrue(more14(new int[]{1, 1}));
     }
 }
--- src/test/java/com/kveola/cb/arrays/two/Only14Test.java
+++ src/test/java/com/kveola/cb/arrays/two/Only14Test.java
@@ -2,11 +2,23 @@ package com.kveola.cb.arrays.two;
 
 import org.junit.jupiter.api.Test;
 
+import static com.kveola.cb.arrays.two.Only14.only14;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Only14Test {
 
     @Test
-    void only14() {
+    void only14TestOne() {
+        assertTrue(only14(new int[]{1, 4, 1, 4}));
+    }
+
+    @Test
+    void only14TestTwo() {
+        assertFalse(only14(new int[]{1, 4, 2, 4}));
+    }
+
+    @Test
+    void only14TestThree() {
+        assertTrue(only14(new int[]{1, 1}));
     }
 }
