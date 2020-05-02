a51bae656a19aa5fb078ab6140b1cf4ae6b80702
olakv <kveola13@student.westerdals.no>
2020-05-03 01:02:29 +0200

    added WordsFrontAlt task

--- src/main/java/com/kveola/cb/ap/one/WordsFront.java
+++ src/main/java/com/kveola/cb/ap/one/WordsFront.java
@@ -2,8 +2,16 @@ package com.kveola.cb.ap.one;
 
 public class WordsFront {
     public static String[] wordsFront(String[] words, int n) {
-        String [] array = new String[n];
+        String[] array = new String[n];
         if (n >= 0) System.arraycopy(words, 0, array, 0, n);
+        return array;
+    }
+
+    public static String[] wordsFrontAlt(String[] words, int n) {
+        String[] array = new String[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = words[i];
+        }
         return array;
     }
 }
--- src/test/java/com/kveola/cb/ap/one/WordsFrontTest.java
+++ src/test/java/com/kveola/cb/ap/one/WordsFrontTest.java
@@ -3,6 +3,7 @@ package com.kveola.cb.ap.one;
 import org.junit.jupiter.api.Test;
 
 import static com.kveola.cb.ap.one.WordsFront.wordsFront;
+import static com.kveola.cb.ap.one.WordsFront.wordsFrontAlt;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordsFrontTest {
@@ -20,5 +21,20 @@ class WordsFrontTest {
     @Test
     void wordsFrontTestThree() {
         assertArrayEquals(new String[]{"a", "b", "c"}, wordsFront(new String[]{"a", "b", "c", "d"}, 3));
+    }
+
+    @Test
+    void wordsFrontAltAltTestOne() {
+        assertArrayEquals(new String[]{"a"}, wordsFrontAlt(new String[]{"a", "b", "c", "d"}, 1));
+    }
+
+    @Test
+    void wordsFrontAltTestTwo() {
+        assertArrayEquals(new String[]{"a", "b"}, wordsFrontAlt(new String[]{"a", "b", "c", "d"}, 2));
+    }
+
+    @Test
+    void wordsFrontAltTestThree() {
+        assertArrayEquals(new String[]{"a", "b", "c"}, wordsFrontAlt(new String[]{"a", "b", "c", "d"}, 3));
     }
 }
