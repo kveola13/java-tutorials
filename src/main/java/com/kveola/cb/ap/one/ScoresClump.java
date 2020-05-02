051fc379061baebbb9a37ec4d6e7cfaa5116db94
olakv <kveola13@student.westerdals.no>
2020-05-02 11:42:41 +0200

    added ScoresClump task

--- src/main/java/com/kveola/cb/ap/one/ScoresClump.java
+++ src/main/java/com/kveola/cb/ap/one/ScoresClump.java
@@ -2,6 +2,9 @@ package com.kveola.cb.ap.one;
 
 public class ScoresClump {
     public static boolean scoresClump(int[] scores) {
-        return true;
+        for (int i = 0; i < scores.length - 2; i++) {
+            if (Math.abs(scores[i+1]-scores[i]) <= 2 && Math.abs(scores[i+2]-scores[i+1]) <= 2 && Math.abs(scores[i+2]-scores[i]) <= 2) return true;
+        }
+        return false;
     }
 }
