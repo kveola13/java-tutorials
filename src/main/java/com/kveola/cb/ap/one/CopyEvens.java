57f040f0fb45bc1d6034118cd98b553652f691ae
olakv <kveola13@student.westerdals.no>
2020-05-03 17:35:46 +0200

    refactoring

--- src/main/java/com/kveola/cb/ap/one/CopyEvens.java
+++ src/main/java/com/kveola/cb/ap/one/CopyEvens.java
@@ -4,10 +4,10 @@ public class CopyEvens {
     public static int[] copyEvens(int[] nums, int count) {
         int[] returnArray = new int[count];
         int countEvens = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int num : nums) {
             if (countEvens < count) {
-                if (nums[i] % 2 == 0) {
-                    returnArray[countEvens] = nums[i];
+                if (num % 2 == 0) {
+                    returnArray[countEvens] = num;
                     countEvens++;
                 }
             }
