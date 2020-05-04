5ca7081cbbede90ac0a7786bede1a4ac00d3502e
olakv <kveola13@student.westerdals.no>
2020-05-04 16:02:06 +0200

    refactoring

--- src/main/java/com/kveola/cb/ap/one/CopyEndy.java
+++ src/main/java/com/kveola/cb/ap/one/CopyEndy.java
@@ -4,10 +4,10 @@ public class CopyEndy {
     public static int[] copyEndy(int[] nums, int count) {
         int countEndy = 0;
         int[] returnArray = new int[count];
-        for (int i = 0; i < nums.length; i++) {
+        for (int num : nums) {
             if (countEndy < count) {
-                if (nums[i] <= 10 && nums[i] >= 0 || nums[i] <= 100 && nums[i] >= 90) {
-                    returnArray[countEndy] = nums[i];
+                if (num <= 10 && num >= 0 || num <= 100 && num >= 90) {
+                    returnArray[countEndy] = num;
                     countEndy++;
                 }
             }
