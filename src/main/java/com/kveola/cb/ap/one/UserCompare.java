fa6236aaed26508669acdd208188c836cb173ecc
olakv <kveola13@student.westerdals.no>
2020-05-05 17:16:26 +0200

    refactoring

--- src/main/java/com/kveola/cb/ap/one/UserCompare.java
+++ src/main/java/com/kveola/cb/ap/one/UserCompare.java
@@ -2,11 +2,12 @@ package com.kveola.cb.ap.one;
 
 public class UserCompare {
     public static int userCompare(String aName, int aId, String bName, int bId) {
-        if (aName.compareTo(bName) < 0) return -1;
-        if (aName.equals(bName) && aId == bId) return 0;
-        if(aName.equals(bName) && aId < bId) return -1;
-        else {
-            return 1;
+        if(aName.equals(bName)){
+            return Integer.compare(aId, bId);
         }
+        if(aName.compareTo(bName) < 0) {
+            return -1;
+        }
+        return 1;
     }
 }
