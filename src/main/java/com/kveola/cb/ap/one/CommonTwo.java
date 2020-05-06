6be1300281ef02a2ffbe076f82c365b415d34687
olakv <kveola13@student.westerdals.no>
2020-05-06 23:36:15 +0200

    finished CommonTwo task

--- src/main/java/com/kveola/cb/ap/one/CommonTwo.java
+++ src/main/java/com/kveola/cb/ap/one/CommonTwo.java
@@ -7,8 +7,8 @@ import java.util.Set;
 public class CommonTwo {
     public static int commonTwo(String[] a, String[] b) {
         int count = 0;
-        Set<String> setA = new HashSet<String>(Arrays.asList(a));
-        Set<String> setB = new HashSet<String>(Arrays.asList(b));
+        Set<String> setA = new HashSet<>(Arrays.asList(a));
+        Set<String> setB = new HashSet<>(Arrays.asList(b));
         for (String s : setA) {
             if (setB.contains(s)) count++;
         }
