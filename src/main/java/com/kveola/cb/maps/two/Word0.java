28ab43cdf8a9df94e5087204b6a6ebcce0e4b60e
olakv <kveola13@student.westerdals.no>
2020-05-07 23:03:26 +0200

    added Word0

--- src/main/java/com/kveola/cb/maps/two/Word0.java
+++ src/main/java/com/kveola/cb/maps/two/Word0.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Word0 {
-    public Map<String, Integer> word0(String[] strings) {
-        return new HashMap<>();
+    public static Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> returnMap = new HashMap<>();
+        for (String s : strings) {
+            System.out.printf("Trying to add %s, is it already here? %s\n", s, returnMap.containsKey(s));
+            returnMap.put(s, 0);
+        }
+        return returnMap;
     }
 }
--- src/test/java/com/kveola/cb/maps/two/Word0Test.java
+++ src/test/java/com/kveola/cb/maps/two/Word0Test.java
@@ -2,11 +2,29 @@ package com.kveola.cb.maps.two;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.kveola.cb.maps.two.Word0.word0;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Word0Test {
 
     @Test
     void word0TestOne() {
+        Map<String, Integer> map = Map.of("a", 0, "b", 0);
+        assertEquals(map, word0(new String[]{"a", "b", "a", "b"}));
+    }
+
+    @Test
+    void word0TestTwo() {
+        Map<String, Integer> map = Map.of("a", 0, "b", 0, "c", 0);
+        assertEquals(map, word0(new String[]{"a", "b", "a", "c", "b"}));
+    }
+
+    @Test
+    void word0TestThree() {
+        Map<String, Integer> map = Map.of("a", 0, "b", 0, "c", 0);
+        assertEquals(map, word0(new String[]{"c", "b", "a"}));
     }
 }
