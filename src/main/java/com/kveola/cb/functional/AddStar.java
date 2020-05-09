af1a63e2cf22c8e07c4fa953d6af341d0f5248f4
olakv <kveola13@student.westerdals.no>
2020-05-09 12:24:35 +0200

    added AddStar task

---
+++ src/main/java/com/kveola/cb/functional/AddStar.java
@@ -0,0 +1,10 @@
+package com.kveola.cb.functional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddStar {
+    public static List<String> addStar(List<String> strings) {
+        return strings.stream().map(s -> s + "*").collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/kveola/cb/functional/AddStarTest.java
@@ -0,0 +1,32 @@
+package com.kveola.cb.functional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.kveola.cb.functional.AddStar.addStar;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddStarTest {
+
+    @Test
+    void AddStarTestOne() {
+        List<String> init = List.of("a", "bb", "ccc");
+        List<String> added = List.of("a*", "bb*", "ccc*");
+        assertEquals(added, addStar(init));
+    }
+
+    @Test
+    void AddStarTestTwo() {
+        List<String> init = List.of("hello", "there");
+        List<String> added = List.of("hello*", "there*");
+        assertEquals(added, addStar(init));
+    }
+
+    @Test
+    void AddStarTestThree() {
+        List<String> init = List.of();
+        List<String> added = List.of();
+        assertEquals(added, addStar(init));
+    }
+}
