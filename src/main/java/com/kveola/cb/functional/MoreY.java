51cf6ccc69ff2189b8a358b2f651732f05f36d5e
olakv <kveola13@student.westerdals.no>
2020-05-10 00:25:13 +0200

    added MoreY task

---
+++ src/main/java/com/kveola/cb/functional/MoreY.java
@@ -0,0 +1,10 @@
+package com.kveola.cb.functional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MoreY {
+    public static List<String> moreY(List<String> strings) {
+        return strings.stream().map(s -> "y" + s + "y").collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/kveola/cb/functional/MoreYTest.java
@@ -0,0 +1,32 @@
+package com.kveola.cb.functional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.kveola.cb.functional.MoreY.moreY;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoreYTest {
+
+    @Test
+    void MoreYTestOne() {
+        List<String> init = List.of("a", "b", "c");
+        List<String> done = List.of("yay", "yby", "ycy");
+        assertEquals(done, moreY(init));
+    }
+
+    @Test
+    void MoreYTestTwo() {
+        List<String> init = List.of("hello", "there");
+        List<String> done = List.of("yhelloy", "ytherey");
+        assertEquals(done, moreY(init));
+    }
+
+    @Test
+    void MoreYTestThree() {
+        List<String> init = List.of("yay");
+        List<String> done = List.of("yyayy");
+        assertEquals(done, moreY(init));
+    }
+}
