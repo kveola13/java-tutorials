c4f64d0257f3b7b16f27f9fa566589d55872cb4a
olakv <kveola13@student.westerdals.no>
2020-05-09 12:16:32 +0200

    added Doubling task

---
+++ src/main/java/com/kveola/cb/functional/Doubling.java
@@ -0,0 +1,10 @@
+package com.kveola.cb.functional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Doubling {
+    public static List<Integer> doubling(List<Integer> nums) {
+        return nums.stream().map(n -> n * 2).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/kveola/cb/functional/DoublingTest.java
@@ -0,0 +1,32 @@
+package com.kveola.cb.functional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.kveola.cb.functional.Doubling.doubling;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublingTest {
+
+    @Test
+    void DoublingTestOne() {
+        List<Integer> init = List.of(1, 2, 3);
+        List<Integer> doubled = List.of(2, 4, 6);
+        assertEquals(doubled, doubling(init));
+    }
+
+    @Test
+    void DoublingTestTwo() {
+        List<Integer> init = List.of(6, 8, 6, 8, -1);
+        List<Integer> doubled = List.of(12, 16, 12, 16, -2);
+        assertEquals(doubled, doubling(init));
+    }
+
+    @Test
+    void DoublingTestThree() {
+        List<Integer> init = List.of();
+        List<Integer> doubled = List.of();
+        assertEquals(doubled, doubling(init));
+    }
+}
