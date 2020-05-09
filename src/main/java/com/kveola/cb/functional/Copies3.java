5db6e59561f467a2934b5fabe7db1e2b01411251
olakv <kveola13@student.westerdals.no>
2020-05-09 12:28:47 +0200

    added Copies3 alt task

--- src/main/java/com/kveola/cb/functional/Copies3.java
+++ src/main/java/com/kveola/cb/functional/Copies3.java
@@ -7,4 +7,8 @@ public class Copies3 {
     public static List<String> copies3(List<String> strings) {
         return strings.stream().map(s -> s.repeat(3)).collect(Collectors.toList());
     }
+
+    public static List<String> copies3Alt(List<String> strings) {
+        return strings.stream().map(s -> s + s + s).collect(Collectors.toList());
+    }
 }
--- src/test/java/com/kveola/cb/functional/Copies3Test.java
+++ src/test/java/com/kveola/cb/functional/Copies3Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static com.kveola.cb.functional.Copies3.copies3;
+import static com.kveola.cb.functional.Copies3.copies3Alt;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Copies3Test {
@@ -28,5 +29,26 @@ class Copies3Test {
         List<String> init = List.of("hello", "there");
         List<String> copied = List.of("hellohellohello", "theretherethere");
         assertEquals(copied, copies3(init));
+    }
+
+    @Test
+    void Copies3AltTestOne() {
+        List<String> init = List.of("a", "bb", "ccc");
+        List<String> copied = List.of("aaa", "bbbbbb", "ccccccccc");
+        assertEquals(copied, copies3Alt(init));
+    }
+
+    @Test
+    void Copies3AltTestTwo() {
+        List<String> init = List.of("24", "a", "");
+        List<String> copied = List.of("242424", "aaa", "");
+        assertEquals(copied, copies3Alt(init));
+    }
+
+    @Test
+    void Copies3AltTestThree() {
+        List<String> init = List.of("hello", "there");
+        List<String> copied = List.of("hellohellohello", "theretherethere");
+        assertEquals(copied, copies3Alt(init));
     }
 }
