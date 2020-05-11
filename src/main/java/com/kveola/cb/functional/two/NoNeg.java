da32e4647aaeda700165f131454745eab5093ef7
olakv <kveola13@student.westerdals.no>
2020-05-11 16:07:58 +0200

    added NoNeg task

--- src/main/java/com/kveola/cb/functional/two/NoNeg.java
+++ src/main/java/com/kveola/cb/functional/two/NoNeg.java
@@ -1,11 +1,10 @@
 package com.kveola.cb.functional.two;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NoNeg {
     public static List<Integer> noNeg(List<Integer> nums) {
-        List<Integer> returnList = List.of();
-
-        return returnList;
+        return nums.stream().filter(n -> n > 0).collect(Collectors.toList());
     }
 }
